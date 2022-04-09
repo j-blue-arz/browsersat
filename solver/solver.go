2c24229a80ec708d6580a698400f6bdee7be186b
Julian Arz <j.arz@web.de>
2022-04-09 23:20:03 +0200

    compiles to webassembly

--- solver/solver.go
+++ solver/solver.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/crillab/gophersat/bf"
 	"strings"
+	"syscall/js"
+
+	"github.com/crillab/gophersat/bf"
 )
 
 func IsSat(inputFormula string) (bool, error) {
@@ -14,4 +16,24 @@ func IsSat(inputFormula string) (bool, error) {
 	}
 	model := bf.Solve(formula)
 	return model != nil, nil
+}
+
+func jsonWrapper() js.Func {
+	jsonFunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		if len(args) != 1 {
+			return "Invalid no of arguments passed"
+		}
+		isSat, err := IsSat(args[0].String())
+		if err != nil {
+			fmt.Printf("IsSat returned error: %s\n", err)
+			return err.Error()
+		}
+		return isSat
+	})
+	return jsonFunc
+}
+
+func main() {
+	js.Global().Set("isSat", jsonWrapper())
+	<-make(chan bool)
 }
