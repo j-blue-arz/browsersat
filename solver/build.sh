8cd32bc5c12324666ac099191e990d33fe3087c4
Julian Arz <j.arz@web.de>
2022-04-17 00:48:33 +0200

    small cleanup

--- solver/build.sh
+++ solver/build.sh
@@ -1,2 +1,2 @@
 GOOS=js GOARCH=wasm go build -o solver.wasm
-cp solver.wasm ../client/public/
+mv solver.wasm ../client/public/
--- solver/main.go
+++ solver/main.go
@@ -6,22 +6,20 @@ import (
 	"syscall/js"
 )
 
-func jsonWrapper() js.Func {
-	jsonFunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		if len(args) != 1 {
-			return "Invalid no of arguments passed"
-		}
-		isSat, err := sat.IsSat(args[0].String())
-		if err != nil {
-			fmt.Printf("IsSat returned error: %s\n", err)
-			return err.Error()
-		}
-		return isSat
-	})
-	return jsonFunc
+func solveFormula(this js.Value, args []js.Value) interface{} {
+	if len(args) != 1 {
+		return "Invalid no of arguments passed"
+	}
+	result, err := sat.IsSat(args[0].String())
+	if err != nil {
+		fmt.Printf("IsSat returned error: %s\n", err)
+		return err.Error()
+	}
+
+	return result
 }
 
 func main() {
-	js.Global().Set("solveFormula", jsonWrapper())
+	js.Global().Set("solveFormula", js.FuncOf(solveFormula))
 	<-make(chan bool)
 }
