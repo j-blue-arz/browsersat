1016015dc1dce28e5ab70f6340ce5935f9151688
Julian Arz <j.arz@web.de>
2022-04-17 23:55:09 +0200

    fixes test
    extract sat from map, cast to bool

--- solver/sat/solver_test.go
+++ solver/sat/solver_test.go
@@ -4,14 +4,16 @@ import "testing"
 
 func TestLiteralSat(t *testing.T) {
 	result, _ := IsSat("x")
-	if !result {
+	sat, ok := result["sat"].(bool)
+	if !ok || !sat {
 		t.Fail()
 	}
 }
 
 func TestFormulaUnsat(t *testing.T) {
 	result, _ := IsSat("x & ^x")
-	if result {
+	sat, ok := result["sat"].(bool)
+	if !ok || sat {
 		t.Fail()
 	}
 }
