c636acdeafe8983fe5997bfb9bee57085671ad4f
Julian Arz <j.arz@web.de>
2022-05-13 23:05:29 +0200

    adds tinygo wasm build

--- solver/build.sh
+++ solver/build.sh
@@ -1,2 +1,13 @@
-GOOS=js GOARCH=wasm go build -o solver.wasm wasm/golang/main.go
-mv solver.wasm ../client/public/
+#/bin/bash
+
+if [ "$1" = "golang" ]; then
+    GOOS=js GOARCH=wasm go build -o solver.wasm wasm/golang/main.go
+    mv solver.wasm ../client/public/
+elif [ "$1" = "tinygo" ]; then
+    tinygo build -o solver.wasm -target wasm wasm/tinygo/main.go
+    mv solver.wasm ../client/public/
+else
+    echo "Usage: ./build.sh golang or ./build.sh tinygo"
+fi
+
+
