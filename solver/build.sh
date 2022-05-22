10d1829e0e20054c03db63e3c00984a392a85725
Julian Arz <j.arz@web.de>
2022-05-22 23:18:04 +0200

    adjusts index.html to work with tinygo

--- solver/build.sh
+++ solver/build.sh
@@ -1,11 +1,21 @@
 #/bin/bash
 
-if [ "$1" = "golang" ]; then
+build_wasm_golang() {
     GOOS=js GOARCH=wasm go build -o solver.wasm wasm/golang/main.go
-    mv solver.wasm ../client/public/
-elif [ "$1" = "tinygo" ]; then
+    mv solver.wasm ../client/public/solver_golang.wasm
+    cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" ../client/public/wasm_exec_golang.js
+}
+
+build_wasm_tinygo() {
     tinygo build -o solver.wasm -target wasm wasm/tinygo/main.go
-    mv solver.wasm ../client/public/
+    mv solver.wasm ../client/public/solver.wasm
+    cp $(tinygo env TINYGOROOT)/targets/wasm_exec.js ../client/public/wasm_exec.js
+}
+
+if [ "$1" = "golang" ]; then
+    build_wasm_golang
+elif [ "$1" = "tinygo" ]; then
+    build_wasm_tinygo
 else
     echo "Usage: ./build.sh golang or ./build.sh tinygo"
 fi
