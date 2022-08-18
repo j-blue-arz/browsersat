0c57696793d82088b93ee3988f167fc995c102e1
Julian Arz <j.arz@web.de>
2022-08-18 23:10:36 +0200

    delay loading wasm until app was mounted
    * goal: no parallel loading of wasm and app, to speed up app load/paint
    * needed because tinygo cannot be supported, and wasm size is ~4mb

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,24 @@ import { Constraints } from "./Constraints.jsx";
 import { Header } from "./Header.jsx";
 import "./App.css";
 
-function App() {
-    return (
-        <div className="app__wrapper">
-            <div className="app__content">
-                <Header />
-                <Constraints />
-            </div>
-        </div>
-    );
-}
+export default class App extends React.Component {
+    componentDidMount() {
+        WebAssembly.instantiateStreaming(fetch(process.env.PUBLIC_URL + "/solver.wasm"), window.go.importObject).then(
+            (obj) => {
+                window.go.run(obj.instance);
+                window.satsolver.initializeSolver();
+            }
+        );
+    }
 
-export default App;
+    render() {
+        return (
+            <div className="app__wrapper">
+                <div className="app__content">
+                    <Header />
+                    <Constraints />
+                </div>
+            </div>
+        );
+    }
+}
